3b2468d2f2d750cad26c0c711595c3dd01219d93
XI <khchow@xandi.co.jp>
2021-03-26 14:10:08 +0600

    migration added for table MST_EMPLOYEE

---
+++ app/Database/Migrations/2021-03-26-071129_MstEmployee.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class MstEmployee extends Migration
+{
+	public function up()
+	{
+        $this->forge->addField([
+            'employee_id'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '20',
+                'null'           => false,
+                'comment'        => '社員ID / Employee ID',
+            ],
+            'employee_name'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '200',
+                'null'       => false,
+                'comment'    => '社員名 / Employee Name',
+            ],
+            'employee_name_kana' => [
+                'type'   => 'VARCHAR',
+                'constraint'=> '200',
+                'null'   => false,
+                'comment'=> '社員名カナ / Employee Name Kana',
+            ],
+            'update_date' => [
+                'type'   => 'DATETIME',
+                'null'   => false,
+                'comment'=> '更新日 / Update Date',
+            ],
+            'update_user_id' => [
+                'type'   => 'VARCHAR',
+                'constraint'=> '15',
+                'null'   => false,
+                'comment'=> '更新者 / Update By',
+            ],
+            'insert_date' => [
+                'type'   => 'DATETIME',
+                'null'   => false,
+                'comment'=> '作成日 / Create Date',
+            ],
+            'insert_user_id' => [
+                'type'   => 'VARCHAR',
+                'constraint'=> '15',
+                'null'   => false,
+                'comment'=> '作成者 / Create By',
+            ],
+            'delete_flag' => [
+                'type'   => 'BOOLEAN',
+                'null'   => false,
+                'comment'=> '削除フラグ / Delete Flag',
+            ],
+        ]);
+        $this->forge->addKey('employee_id', true);
+        $this->forge->createTable('mst_employee');
+	}
+
+	public function down()
+	{
+        $this->forge->dropTable('mst_employee');
+	}
+}
