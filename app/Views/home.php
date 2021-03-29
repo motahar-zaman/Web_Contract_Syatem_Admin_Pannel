09544a9280e68c226d184fd03f8d23b062381054
XI <khchow@xandi.co.jp>
2021-03-29 16:30:54 +0600

    add landing/home page after successful login

---
+++ app/Views/home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css\style.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<!--
+<h2>Login Form</h2> -->
+<div class="header">
+    <p>WEB契約システム</p>
+    <h1>メニュー</h1>
+    <p class="mini">	[ユーザーID]：[ユーザー名]</p>
+</div>
+<div class="data">
+    <table class="menuTable">
+
+        <th class="tableHead">
+            契約機能
+        </th>
+        <th class="tableHead">
+
+        </th>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <th class="tableHead">
+            契約機能
+        </th>
+        <th class="tableHead">
+
+        </th>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+        <tr>
+            <td>契約検索	</td>
+            <td>契約情報の検索を行い、画面表示を行います	</td>
+        </tr>
+    </table>
+</div>
+
+
+<footer class="loginFooter">
+    <p class="footertext">アクセス日時：YYYY/MM/DD	</p>
+</footer>
+
+</body>
+</html>
