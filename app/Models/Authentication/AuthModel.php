aceb6437bb267689b0762af908c2882a74098b09
XI <khchow@xandi.co.jp>
2021-03-30 15:43:10 +0600

    database field name correction

--- app/Models/Authentication/AuthModel.php
+++ app/Models/Authentication/AuthModel.php
@@ -15,7 +15,7 @@ class AuthModel
 
     public function loginContractor($userId, $password){
         $queryString = "SELECT contractor_id, contractor_name, contractor_name_kana, zipcode, address_01, address_02, tel_no, fax_no, mail_address, type_contractor, update_date,
-                        update_user_id, insert_date, insert_user_id, delete_flag  FROM mst_contractor WHERE employee_id = '$userId'  AND delete_flag = 1 ";
+                        update_user_id, insert_date, insert_user_id, delete_flag  FROM mst_contractor WHERE contractor_id = '$userId'  AND delete_flag = 1 ";
         $userData = (new Database())->queryExecution($queryString);
         return $userData;
     }
