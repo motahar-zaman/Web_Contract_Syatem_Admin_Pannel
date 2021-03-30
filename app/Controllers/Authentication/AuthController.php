8c7dbf53d47160eac9c092b825ebe8afb5bc98d1
XI <khchow@xandi.co.jp>
2021-03-30 15:44:48 +0600

    user login for k1_employee and contractor done with session creation

--- app/Controllers/Authentication/AuthController.php
+++ app/Controllers/Authentication/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Controllers\Authentication;
 use App\Controllers\BaseController;
 use App\Models\Authentication\AuthModel;
 use CodeIgniter\HTTP\Request;
+use Config\Services;
 
 class AuthController extends BaseController{
     public function index()    {
@@ -18,19 +19,49 @@ class AuthController extends BaseController{
         $user = explode("_",$name);
         $auth = new AuthModel();
 
-        if($user[0] == k1_employee_user_prefix){
+        if ($user[0] == k1_employee_user_prefix){
             $userInfo = $auth->loginEmployee($name, $password);
-            if($userInfo){
+            if (isset($userInfo) && count($userInfo) > 0){
+                $userInfo = $userInfo[0];
+
+                $session_data = array(
+                    'employeeId' => $userInfo->employee_id,
+                    'employeeName' => $userInfo->employee_name,
+                    'employeeNameKana' => $userInfo->employee_name_kana,
+                    'updateUserId' => $userInfo->update_user_id,
+                    'insertUserId' => $userInfo->insert_user_id
+                );
+
+                Services::session()->set($session_data);
+
                 return redirect()->to("/home");
             }
             else{
                 return redirect()->to('/login');
             }
-
         }
-        if($user[0] == contractor_user_prefix){
+        elseif ($user[0] == contractor_user_prefix){
             $userInfo = $auth->loginContractor($name, $password);
-            if($userInfo){
+            if(isset($userInfo) && count($userInfo) > 0){
+                $userInfo = $userInfo[0];
+
+                $session_data = array(
+                    'contractorId' => $userInfo->contractor_id,
+                    'contractorName' => $userInfo->contractor_name,
+                    'contractorNameKana' => $userInfo->contractor_name_kana,
+                    'zipcode' => $userInfo->zipcode,
+                    'address_01' => $userInfo->address_01,
+                    'address_02' => $userInfo->address_02,
+                    'telNo' => $userInfo->tel_no,
+                    'faxNo' => $userInfo->fax_no,
+                    'mailAddress' => $userInfo->mail_address,
+                    'typeContractor' => $userInfo->type_contractor,
+                    'updateUserId' => $userInfo->update_user_id,
+                    'insertUserId' => $userInfo->insert_user_id
+                );
+
+                Services::session()->set($session_data);
+
                 return redirect()->to("/home");
             }
             else{
