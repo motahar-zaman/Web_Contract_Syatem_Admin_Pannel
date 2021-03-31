2b922d637f32ec832ae20fc0c6b257632cf9717b
XI <khchow@xandi.co.jp>
2021-03-31 11:47:39 +0600

    '/' route added to login page as landing/home page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,6 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
 
 /*
  * --------------------------------------------------------------------
@@ -51,7 +50,7 @@ if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
 {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
-
+$routes->get('/', 'Authentication\AuthController');
 $routes->get('login', 'Authentication\AuthController');
 $routes->post('login', 'Authentication\AuthController::loginAction');
 $routes->get('logout', 'Authentication\AuthController::logout');
