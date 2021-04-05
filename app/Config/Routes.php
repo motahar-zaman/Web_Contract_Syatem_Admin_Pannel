283460c4b4341ea391fc43fb584f3b2c084172cc
XI <khchow@xandi.co.jp>
2021-04-05 16:43:53 +0900

    Revert "create routes for contractor registration"
    This reverts commit 0a248c831c38ae06fb5a07eec9c15626d52bd504

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -50,12 +50,8 @@ if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
 {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
-
-$routes->get('/', 'Authentication\AuthController::index');
+$routes->get('/', 'Authentication\AuthController');
 $routes->get('login', 'Authentication\AuthController::login');
 $routes->post('login', 'Authentication\AuthController::loginAction');
 $routes->get('logout', 'Authentication\AuthController::logout');
 $routes->get('home', 'Authentication\AuthController::home');
-
-$routes->get('temp-contractor-registration', 'Contractor\RegistrationController::index');
-$routes->post('temp-contractor-registration', 'Contractor\RegistrationController::registrationAction');
