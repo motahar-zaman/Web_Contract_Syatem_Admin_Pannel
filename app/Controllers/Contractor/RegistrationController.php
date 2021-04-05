6d00976220f9f3440e2d175c889028aa378f56d8
XI <khchow@xandi.co.jp>
2021-04-05 13:58:09 +0600

    create Controller methods for contract registration

--- app/Controllers/Contractor/RegistrationController.php
+++ app/Controllers/Contractor/RegistrationController.php
@@ -8,7 +8,7 @@ class RegistrationController extends BaseController
 {
     public function index(){
         if( session() && session()->get('login') ){
-            return view("template/pages/forms/contractor", ["title" => "Contractor Registration"]);
+            return view("template/pages/forms/temp_contractor", ["title" => "Temporary Contractor Registration"]);
         }
         else{
             return redirect()->to("/login");
@@ -16,6 +16,23 @@ class RegistrationController extends BaseController
     }
 
     public function registrationAction(){
+        if( session() && session()->get('login') ){
+        }
+        else{
+            return redirect()->to("/login");
+        }
+    }
+
+    public function tempRegistration(){
+        if( session() && session()->get('login') ){
+            return view("template/pages/forms/contractor", ["title" => "Contractor Registration"]);
+        }
+        else{
+            return redirect()->to("/login");
+        }
+    }
+
+    public function tempRegistrationAction(){
         if( session() && session()->get('login') ){
         }
         else{
