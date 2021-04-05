9191a7767002033fb65619bbc7c9ac5a8dca9759
XI <khchow@xandi.co.jp>
2021-04-05 12:37:54 +0600

    dynamic page title sent from controller

--- app/Controllers/Authentication/AuthController.php
+++ app/Controllers/Authentication/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends BaseController{
 
     public function index(){
         if( session() && session()->get('login') ){
-            return view("template/pages/tables/home");
+            return view("template/pages/tables/home", ["title" => "Home"]);
         }
         else{
             return redirect()->to("/login");
@@ -19,7 +19,7 @@ class AuthController extends BaseController{
     }
 
     public function login(){
-        return view("template/pages/examples/login");
+        return view("template/pages/examples/login", ["title" => "Login"]);
     }
 
     public function loginAction(){
