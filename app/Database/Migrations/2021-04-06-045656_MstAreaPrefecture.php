3702b01c260ed5307c081da3016b235dd6a11086
XI <khchow@xandi.co.jp>
2021-04-06 12:19:49 +0600

    MST_AREA_PREFECTURE table migration created

---
+++ app/Database/Migrations/2021-04-06-045656_MstAreaPrefecture.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class MstAreaPrefecture extends Migration
+{
+	public function up()
+	{
+        $this->forge->addField([
+            'area_id'            => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '10',
+                'null'           => false,
+                'comment'        => 'エリアID',
+            ],
+            'district_id'        => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '10',
+                'null'           => false,
+                'comment'        => '地方ID',
+            ],
+            'area_name'      => [
+                'type'       => 'VARCHAR',
+                'constraint' => '10',
+                'null'       => false,
+                'comment'    => '名称',
+            ],
+            'area_areas'    => [
+                'type'      => 'VARCHAR',
+                'constraint'=> '10',
+                'null'      => false,
+                'comment'   => '名称略',
+            ],
+            'sort_order'         => [
+                'type'           => 'INTEGER',
+                'constraint'     => 5,
+                'null'           => false,
+                'comment'        => '表示順',
+            ],
+            'update_date' => [
+                'type'    => 'DATETIME',
+                'null'    => false,
+                'comment' => '更新日',
+            ],
+            'update_user_id'     => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '15',
+                'null'           => false,
+                'comment'        => '更新者',
+            ],
+            'insert_date'    => [
+                'type'       => 'DATETIME',
+                'null'       => false,
+                'comment'    => '作成日',
+            ],
+            'insert_user_id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '15',
+                'null'       => false,
+                'comment'    => '作成者',
+            ],
+            'delete_flag'    => [
+                'type'       => 'BOOLEAN',
+                'null'       => false,
+                'comment'    => '削除フラグ',
+            ],
+        ]);
+        $this->forge->addKey('area_id', true);
+        $this->forge->createTable('MST_AREA_PREFECTURE');
+	}
+
+	public function down()
+	{
+        $this->forge->dropTable('MST_AREA_PREFECTURE');
+	}
+}
