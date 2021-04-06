6df05396151bb284418bfe1d382ee96c7f5bad90
XI <khchow@xandi.co.jp>
2021-04-06 12:19:01 +0600

    MST_AREA_SMALL table migration created

---
+++ app/Database/Migrations/2021-04-06-045445_MstAreaSmall.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class MstAreaSmall extends Migration
+{
+	public function up()
+	{
+        $this->forge->addField([
+            'area_id'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '10',
+                'null'           => false,
+                'comment'        => 'エリアID',
+            ],
+            'district_id'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '10',
+                'null'           => false,
+                'comment'        => '地方ID',
+            ],
+            'prefecture_id'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '10',
+                'null'           => false,
+                'comment'        => '都道府県ID',
+            ],
+            'area_large_id'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '10',
+                'null'           => false,
+                'comment'        => 'エリア大ID',
+            ],
+            'area_name'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => false,
+                'comment'    => '名称',
+            ],
+            'area_areas'=> [
+                'type'   => 'VARCHAR',
+                'constraint'=> '100',
+                'null'   => false,
+                'comment'=> '名称略',
+            ],
+	        'sort_order'          => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'null'           => false,
+                'comment'        => '表示順',
+            ],
+            'update_date' => [
+                'type'   => 'DATETIME',
+                'null'   => false,
+                'comment'=> '更新日',
+            ],
+	        'update_user_id'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '15',
+                'null'           => false,
+                'comment'        => '更新者',
+            ],
+            'insert_date'       => [
+                'type'       => 'DATETIME',
+                'null'       => false,
+                'comment'    => '作成日',
+            ],
+            'insert_user_id' => [
+                'type'   => 'VARCHAR',
+                'constraint'=> '15',
+                'null'   => false,
+                'comment'=> '作成者',
+            ],
+	        'delete_flag'          => [
+                'type'           => 'BOOLEAN',
+                'null'           => false,
+                'comment'        => '削除フラグ',
+            ],
+        ]);
+        $this->forge->addKey('area_id', true);
+        $this->forge->createTable('MST_AREA_SMALL');
+	}
+
+	public function down()
+	{
+        $this->forge->dropTable('MST_AREA_SMALL');
+	}
+}
