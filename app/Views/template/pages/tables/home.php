3dd04399835942f61eb4a9a3beeb5bc798a2bdfd
XI <khchow@xandi.co.jp>
2021-04-08 10:01:08 +0600

    menu/home page UI correction

--- app/Views/template/pages/tables/home.php
+++ app/Views/template/pages/tables/home.php
@@ -29,34 +29,34 @@
         <div class="underline mt-2"></div>
         <div class="row">
           <div class="menuTable col-5 mt-5 mb-5">
-            <div class="card">
+            <div class="card mb-0">
               <div class="card-body table-responsive p-0">
                 <table class="table table-hover text-nowrap">
                   <thead>
-                  <tr class="bgWheat">
-                    <th>契約機能(contract)</th>
-                    <th></th>
-                  </tr>
+                    <tr class="bgWheat">
+                      <th>契約機能(contract)</th>
+                      <th></th>
+                    </tr>
                   </thead>
                   <tbody>
-                  <tr>
-                    <td><a href="#">契約検索(search)</a></td>
-                    <td>契約情報の検索を行い、画面表示を行います
-                  </tr>
-                  <?php if(session()->get("user") == "employee"){ ?>
                     <tr>
-                      <td><a href="#">契約登録(reg)</a></td>
-                      <td>契約者の新規登録を行います</td>
+                      <td><a href="#">契約検索(search)</a></td>
+                      <td>契約情報の検索を行い、画面表示を行います
                     </tr>
-                    <tr>
-                      <td><a href="#">契約更新(update)</a></td>
-                      <td>契約者の既存契約更新を行います</td>
-                    </tr>
-                    <tr>
-                      <td><a href="#">仮契約登録(temp reg)</a></td>
-                      <td>契約者の仮登録を行います（社員のみ可能）</td>
-                    </tr>
-                  <?php }?>
+                    <?php if(session()->get("user") == "employee"){ ?>
+                      <tr>
+                        <td><a href="#">契約登録(reg)</a></td>
+                        <td>契約者の新規登録を行います</td>
+                      </tr>
+                      <tr>
+                        <td><a href="#">契約更新(update)</a></td>
+                        <td>契約者の既存契約更新を行います</td>
+                      </tr>
+                      <tr>
+                        <td><a href="#">仮契約登録(temp reg)</a></td>
+                        <td>契約者の仮登録を行います（社員のみ可能）</td>
+                      </tr>
+                    <?php }?>
                   </tbody>
                 </table>
                 <table class="table table-hover text-nowrap">
@@ -88,14 +88,11 @@
                   </tbody>
                 </table>
               </div>
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
+            <div class="text-right pr-1">
+              <small>アクセス日時：<?= date("Y/m/d")?>	</small>
+            </div>
           </div>
-        </div>
-        <div class="underline"></div>
-        <div class="text-right pr-5">
-          <p class="pt-3">アクセス日時：<?= date("Y/m/d")?>	</p>
         </div>
       </div>
     </div>
