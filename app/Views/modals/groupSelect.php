b635a6561d5e5cdd09e7a0633646af64979d0507
XI <khchow@xandi.co.jp>
2021-04-09 13:32:11 +0600

    create group select modal UI in views/modals directory

---
+++ app/Views/modals/groupSelect.php
@@ -0,0 +1,85 @@
+<div class="modal fade" id="group-select-modal">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Group Select</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">商品ID（完全一致）</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">商品名（あいまい）</label>
+                            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="">
+                        </div>
+                        <button type="submit" class="btn btn-primary pl-4 pr-4">検索</button>
+                        <button type="submit" class="btn btn-primary ml-2">条件クリア</button>
+                    </div>
+                </form>
+                <br />
+                <div class="card-body table-responsive p-0 ml-1" style="height: 300px;">
+                    <table class="table table-head-fixed text-nowrap ml-1">
+                        <thead>
+                            <tr>
+                                <th>選択</th>
+                                <th>契約者ID</th>
+                                <th>契約者名	</th>
+                                <th>住所</th>
+                                <th>電話番号</th>
+                                <th>メールアドレス</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>選択	</td>
+                                <td>契約者ID</td>
+                                <td>契約者名	</td>
+                                <td>住所	</td>
+                                <td>電話番号	</td>
+                                <td>メールアドレス	</td>
+                            </tr>
+                            <tr>
+                                <td>選択	</td>
+                                <td>契約者ID</td>
+                                <td>契約者名	</td>
+                                <td>住所	</td>
+                                <td>電話番号	</td>
+                                <td>メールアドレス	</td>
+                            </tr>
+                            <tr>
+                                <td>選択	</td>
+                                <td>契約者ID</td>
+                                <td>契約者名	</td>
+                                <td>住所	</td>
+                                <td>電話番号	</td>
+                                <td>メールアドレス	</td>
+                            </tr>
+                            <tr>
+                                <td>選択	</td>
+                                <td>契約者ID</td>
+                                <td>契約者名	</td>
+                                <td>住所	</td>
+                                <td>電話番号	</td>
+                                <td>メールアドレス	</td>
+                            </tr>
+                            <tr>
+                                <td>選択	</td>
+                                <td>契約者ID</td>
+                                <td>契約者名	</td>
+                                <td>住所	</td>
+                                <td>電話番号	</td>
+                                <td>メールアドレス	</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
