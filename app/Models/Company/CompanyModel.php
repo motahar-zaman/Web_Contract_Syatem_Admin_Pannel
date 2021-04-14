52b57b2a760f4f3bb9412371ba71ae38a96ed856
Zaman <motaharz95@gmail.com>
2021-04-14 16:25:48 +0600

    create methods and sql query to access all company data

--- app/Models/Company/CompanyModel.php
+++ app/Models/Company/CompanyModel.php
@@ -32,4 +32,48 @@ class CompanyModel
 
         return (new Database())->queryExecution($queryString);
     }
+
+    public function getAllCompanyData(){
+        $data = $this->getAllData();
+        return $this->mapData($data);
+    }
+
+    public function getAllData(){
+        $queryString = "SELECT company_id, company_name, company_name_kana, daihyousha_name, daihyousha_name_kana, zipcode, address_01, address_02, tel_no,
+                        fax_no,mail_address, site_url, update_date, update_user_id, insert_date, insert_user_id, delete_flag FROM mst_company WHERE delete_flag = 0
+                        ORDER BY update_date DESC";
+
+        return (new Database())->queryExecution($queryString);
+    }
+
+    public function mapData($datas = array()){
+        $length = count($datas);
+        $mappedData = array();
+
+        for($i = 0; $i < $length; $i++){
+            $data = $datas[$i];
+            $company = new Company();
+            if(isset($data)){
+                $company->setId($data->company_id ?? NULL);
+                $company->setName($data->company_name ?? NULL);
+                $company->setNameKana($data->company_name_kana ?? NULL);
+                $company->setRepresentative($data->daihyousha_name ?? NULL);
+                $company->setRepresentativeKana($data->daihyousha_name_kana ?? NULL);
+                $company->setZipCode($data->zipcode ?? NULL);
+                $company->setAddress01($data->address_01 ?? NULL);
+                $company->setAddress02($data->address_02 ?? NULL);
+                $company->setTelNo($data->tel_no ?? NULL);
+                $company->setFaxNo($data->fax_no ?? NULL);
+                $company->setMailAddress($data->mail_address ?? NULL);
+                $company->setSiteUrl($data->site_url ?? NULL);
+                $company->setUpdateDate($data->update_date ?? NULL);
+                $company->setUpdateUserId($data->update_user_id ?? NULL);
+                $company->setInsertDate($data->insert_date ?? NULL);
+                $company->setInsertUserId($data->insert_user_id ?? NULL);
+                $company->setDeleteFlag($data->delete_flag ?? NULL);
+            }
+            array_push($mappedData, $company);
+        }
+        return $mappedData;
+    }
 }
