4a905aa7088b15a18c9b64340eb843847277dbc2
Zaman <motaharz95@gmail.com>
2021-04-16 10:51:16 +0600

    add Query Bindings when execute query for simplifying syntax and producing safer queries

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 class Database
 {
-    public function queryExecution($query){
+    public function queryExecution($query, $parameter = NULL){
         $db = \Config\Database::connect();
-        return $db->query($query)->getResult();
+        return $db->query($query, $parameter)->getResult();
     }
 }
