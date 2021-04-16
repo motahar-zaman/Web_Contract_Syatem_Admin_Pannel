2d5da886c5ed67e9a38a12bc7c8fa2521fe508c0
Zaman <motaharz95@gmail.com>
2021-04-16 15:46:22 +0600

    handle exception/error when query execute

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -2,15 +2,29 @@
 
 namespace App\Models;
 
+use Exception;
+
 class Database
 {
     public function writeQueryExecution($query, $parameter = NULL){
         $db = \Config\Database::connect();
-        return $db->query($query, $parameter);
+        try{
+            return $db->query($query, $parameter);
+        }
+        catch(Exception $e)
+        {
+            return false;
+        }
     }
 
     public function readQueryExecution($query, $parameter = NULL){
         $db = \Config\Database::connect();
-        return $db->query($query, $parameter)->getResult();
+        try{
+            return $db->query($query, $parameter)->getResult();
+        }
+        catch(Exception $e)
+        {
+            return false;
+        }
     }
 }
