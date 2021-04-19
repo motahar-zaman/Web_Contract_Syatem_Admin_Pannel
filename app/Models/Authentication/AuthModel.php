44a83af2b05c84b54f22db3f3b834eb179d4432f
Zaman <motaharz95@gmail.com>
2021-04-19 09:16:29 +0600

    Query Execution method name correction at login

--- app/Models/Authentication/AuthModel.php
+++ app/Models/Authentication/AuthModel.php
@@ -9,14 +9,14 @@ class AuthModel
     public function loginEmployee($userId, $password){
         $queryString = "SELECT employee_id, employee_name, employee_name_kana, update_date, update_user_id, insert_date, insert_user_id, delete_flag 
                         FROM mst_employee WHERE employee_id = '$userId' AND password = '$password' AND delete_flag = 1 ";
-        $userData = (new Database())->queryExecution($queryString);
+        $userData = (new Database())->readQueryExecution($queryString);
         return $userData;
     }
 
     public function loginContractor($userId, $password){
         $queryString = "SELECT contractor_id, contractor_name, contractor_name_kana, zipcode, address_01, address_02, tel_no, fax_no, mail_address, type_contractor, update_date,
                         update_user_id, insert_date, insert_user_id, delete_flag  FROM mst_contractor WHERE contractor_id = '$userId' AND password = '$password'  AND delete_flag = 1 ";
-        $userData = (new Database())->queryExecution($queryString);
+        $userData = (new Database())->readQueryExecution($queryString);
         return $userData;
     }
 }
