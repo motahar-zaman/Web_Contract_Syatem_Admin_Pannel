cece6b7c61ccfdcd9939659711313f13e9031100
Zaman <motaharz95@gmail.com>
2021-04-21 13:56:40 +0600

    text change Contractor Select pop-up modal

--- app/Views/modals/contractorSelect.php
+++ app/Views/modals/contractorSelect.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Contractor Select</h4>
+                <h4 class="modal-title">契約者選択</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -11,11 +11,11 @@
                 <form>
                     <div class="card-body">
                         <div class="form-group col-md-4 pl-0">
-                            <label for="exampleInputEmail1">商品ID（完全一致）</label>
+                            <label for="exampleInputEmail1">契約者ID（完全一致）</label>
                             <input type="text" class="form-control" id="exampleInputEmail1" placeholder="">
                         </div>
                         <div class="form-group col-md-4 pl-0">
-                            <label for="exampleInputPassword1">商品名（あいまい）</label>
+                            <label for="exampleInputPassword1">契約者名（あいまい）</label>
                             <input type="text" class="form-control" id="exampleInputPassword1" placeholder="">
                         </div>
                         <button type="submit" class="btn btn-primary pl-4 pr-4">検索</button>
