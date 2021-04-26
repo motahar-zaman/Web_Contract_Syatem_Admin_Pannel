2217c6cf43be12369ffd84fa219cf992748c89f2
Zaman <motaharz95@gmail.com>
2021-04-26 13:36:20 +0600

    Correction query to get contractor by contractorName, add order by with insert date descending

--- app/Models/Contractor/ContractorModel.php
+++ app/Models/Contractor/ContractorModel.php
@@ -131,7 +131,7 @@ class ContractorModel
         $contractorName = "%".$contractorName."%";
         $queryString = "SELECT contractor_id, contractor_name, contractor_name_kana, password, zipcode, address_01, address_02, tel_no, fax_no,
                         mail_address, company_id, group_id, temporary, type_contractor, update_date, update_user_id, insert_date, insert_user_id,
-                        delete_flag FROM mst_contractor WHERE contractor_name LIKE ? AND delete_flag = ?";
+                        delete_flag FROM mst_contractor WHERE contractor_name LIKE ? AND delete_flag = ? ORDER BY insert_date DESC";
         $queryParameter = array($contractorName, 1);
 
         return (new Database())->readQueryExecution($queryString, $queryParameter);
