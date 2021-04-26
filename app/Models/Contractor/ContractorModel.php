8c7502694d7fdfc6e7aace74e1f7ff852fdb3e62
Zaman <motaharz95@gmail.com>
2021-04-26 13:30:23 +0600

    create method and query to get contractor by contractorName

--- app/Models/Contractor/ContractorModel.php
+++ app/Models/Contractor/ContractorModel.php
@@ -126,4 +126,14 @@ class ContractorModel
 
         return (new Database())->readQueryExecution($queryString, $queryParameter);
     }
+
+    public function getContractorByName($contractorName){
+        $contractorName = "%".$contractorName."%";
+        $queryString = "SELECT contractor_id, contractor_name, contractor_name_kana, password, zipcode, address_01, address_02, tel_no, fax_no,
+                        mail_address, company_id, group_id, temporary, type_contractor, update_date, update_user_id, insert_date, insert_user_id,
+                        delete_flag FROM mst_contractor WHERE contractor_name LIKE ? AND delete_flag = ?";
+        $queryParameter = array($contractorName, 1);
+
+        return (new Database())->readQueryExecution($queryString, $queryParameter);
+    }
 }
