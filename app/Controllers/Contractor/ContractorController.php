202cb511f307e0e7a7cf32384eb6896e1ee8da87
Zaman <motaharz95@gmail.com>
2021-04-26 11:57:19 +0600

    controller work to view page and data for Contractor Search UI

--- app/Controllers/Contractor/ContractorController.php
+++ app/Controllers/Contractor/ContractorController.php
@@ -5,12 +5,14 @@ namespace App\Controllers\Contractor;
 
 
 use App\Controllers\BaseController;
+use App\Models\Contractor\ContractorModel;
 
 class ContractorController extends BaseController
 {
     public function contractorSearch(){
         if( session() && session()->get('login') ){
-            return "Hello, This feature is under Developing";
+            $contractor = (new ContractorModel())->getAllContractorData();
+            return view("contractor/contractorSearch", ["title" => "Contractor Search", "contractor" => $contractor]);
         }
         else{
             return redirect()->to("/login");
