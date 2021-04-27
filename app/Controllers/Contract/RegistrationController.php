f85acc1aa95ba82aedef3e09aafb14eac9adf3e9
XI <khchow@xandi.co.jp>
2021-04-27 16:14:35 +0600

    create index method to contract registration controller to get UI page

--- app/Controllers/Contract/RegistrationController.php
+++ app/Controllers/Contract/RegistrationController.php
@@ -4,6 +4,28 @@
 namespace App\Controllers\Contract;
 
 
-class RegistrationController
+use App\Controllers\BaseController;
+use App\Models\Common\SequenceModel;
+use App\Models\Company\CompanyModel;
+use App\Models\Contractor\ContractorModel;
+use App\Models\Group\GroupModel;
+
+class RegistrationController extends BaseController
 {
+    public function index(){
+        if( session() && session()->get('login') ){
+            $group = (new GroupModel())->getAllGroupData();
+            $company = (new CompanyModel())->getAllCompanyData();
+            $contractor = (new ContractorModel())->getAllContractorData();
+            $contractorId = (new SequenceModel())->getContractorSequence();
+            $companyId = (new SequenceModel())->getCompanySequence();
+            $groupId = (new SequenceModel())->getGroupSequence();
+
+            return view("Contract/contract", ["title" => "Contractor Registration", "group" => $group, "company" => $company,
+                "contractor" => $contractor, "contractorId" => $contractorId, "companyId" => $companyId, "groupId" => $groupId]);
+        }
+        else{
+            return redirect()->to("/login");
+        }
+    }
 }
