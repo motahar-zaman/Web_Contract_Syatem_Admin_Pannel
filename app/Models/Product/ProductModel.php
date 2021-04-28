98a5289bc65cc9083f11dc6b165accaab65b1347
XI <khchow@xandi.co.jp>
2021-04-28 14:16:29 +0600

    Create methods to map all product data with product model

--- app/Models/Product/ProductModel.php
+++ app/Models/Product/ProductModel.php
@@ -57,6 +57,7 @@ class ProductModel
 
     public function getAllProductData(){
         $data = $this->getAllData();
+        return $this->mapData($data);
     }
 
     public function getAllData(){
@@ -68,5 +69,38 @@ class ProductModel
         return (new Database())->readQueryExecution($queryString, $queryParameter);
     }
 
+    public function mapData($datas = array()){
+        if(isset($datas) && is_array($datas)){
+            $length = count($datas);
+            $mappedData = array();
 
+            for($i = 0; $i < $length; $i++){
+                $data = $datas[$i];
+                $product = new Product();
+                if(isset($data)){
+                    $product->setId($data->product_id ?? NULL);
+                    $product->setName($data->product_name ?? NULL);
+                    $product->setNameOfficial($data->product_name_official ?? NULL);
+                    $product->setPrice($data->price ?? NULL);
+                    $product->setStartDate($data->start_date ?? NULL);
+                    $product->setEndDate($data->end_date ?? NULL);
+                    $product->setServiceType($data->service_type ?? NULL);
+                    $product->setProductType($data->product_type ?? NULL);
+                    $product->setCampaignFlag($data->campaign_flag ?? NULL);
+                    $product->setShopType($data->shop_type ?? NULL);
+                    $product->setProductNote($data->product_note ?? NULL);
+                    $product->setUpdateDate($data->update_date ?? NULL);
+                    $product->setUpdateUserId($data->update_user_id ?? NULL);
+                    $product->setInsertDate($data->insert_date ?? NULL);
+                    $product->setInsertUserId($data->insert_date_id ?? NULL);
+                    $product->setDeleteFlag($data->delete_flag ?? NULL);
+                }
+                array_push($mappedData, $product);
+            }
+            return $mappedData;
+        }
+        else{
+            return $datas;
+        }
+    }
 }
