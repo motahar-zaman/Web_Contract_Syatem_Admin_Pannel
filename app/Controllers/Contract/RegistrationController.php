57defd0d089e1544da55d8ae4492382af7bc3566
XI <khchow@xandi.co.jp>
2021-04-28 09:54:07 +0600

    Contract registration methods created (tempRegistration, registrationAction, update, updateAction)

--- app/Controllers/Contract/RegistrationController.php
+++ app/Controllers/Contract/RegistrationController.php
@@ -5,7 +5,6 @@ namespace App\Controllers\Contract;
 
 
 use App\Controllers\BaseController;
-use App\Models\Common\SequenceModel;
 use App\Models\Company\CompanyModel;
 use App\Models\Contractor\ContractorModel;
 use App\Models\Group\GroupModel;
@@ -23,6 +22,57 @@ class RegistrationController extends BaseController
         }
         else{
             return redirect()->to("/login");
+        }
+    }
+
+    public function tempRegistration(){
+        if( session() && session()->get('login') ){
+            $group = (new GroupModel())->getAllGroupData();
+            $company = (new CompanyModel())->getAllCompanyData();
+            $contractor = (new ContractorModel())->getAllContractorData();
+
+            return view("Contract/temp_contract", ["title" => "Temporary Contract Registration", "group" => $group, "company" => $company,
+                "contractor" => $contractor]);
+        }
+        else{
+            return redirect()->to("/login");
+        }
+    }
+
+    public function registrationAction(){
+        if( session() && session()->get('login') ){
+            if($this->request->isAJAX()){
+                return json_encode(['msg' => "Successful", 'status' => 1]);
+            }
+            else{
+                return json_encode(['msg' => "Not an ajax request", 'status' => 2]);
+            }
+        }
+        else{
+            return json_encode(['msg' => "Not Logged in user", 'status' => 3]);
+        }
+    }
+
+    public function update(){
+        if( session() && session()->get('login') ){
+            echo "Hello, This section is under developing";
+        }
+        else{
+            return redirect()->to("/login");
+        }
+    }
+
+    public function updateAction(){
+        if( session() && session()->get('login') ){
+            if($this->request->isAJAX()){
+                return json_encode(['msg' => "Successful", 'status' => 1]);
+            }
+            else{
+                return json_encode(['msg' => "Not an ajax request", 'status' => 2]);
+            }
+        }
+        else{
+            return json_encode(['msg' => "Not Logged in user", 'status' => 3]);
         }
     }
 }
