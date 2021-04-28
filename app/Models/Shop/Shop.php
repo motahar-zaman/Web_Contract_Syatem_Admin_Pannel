810a32b15ac241c32f7932718717e00f1724510d
XI <khchow@xandi.co.jp>
2021-04-28 12:45:40 +0600

    Created Shop Model with its property and getter setter methods to set and access properties

---
+++ app/Models/Shop/Shop.php
@@ -0,0 +1,298 @@
+<?php
+
+
+namespace App\Models\Shop;
+
+
+class Shop
+{
+    private $shopId ;
+    private $shopName;
+    private $shopNameKana;
+    private $zipcode;
+    private $address01;
+    private $address02;
+    private $areaId;
+    private $prefecture;
+    private $telNo;
+    private $faxNo;
+    private $mailAddress;
+    private $siteUrl;
+    private $insertDate;
+    private $insertUserId;
+    private $updateDate;
+    private $updateUserId;
+    private $deleteFlag;
+
+    /**
+     * @return mixed
+     */
+    public function getShopId()
+    {
+        return $this->shopId;
+    }
+
+    /**
+     * @param mixed $shopId
+     */
+    public function setShopId($shopId): void
+    {
+        $this->shopId = $shopId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShopName()
+    {
+        return $this->shopName;
+    }
+
+    /**
+     * @param mixed $shopName
+     */
+    public function setShopName($shopName): void
+    {
+        $this->shopName = $shopName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShopNameKana()
+    {
+        return $this->shopNameKana;
+    }
+
+    /**
+     * @param mixed $shopNameKana
+     */
+    public function setShopNameKana($shopNameKana): void
+    {
+        $this->shopNameKana = $shopNameKana;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * @param mixed $zipcode
+     */
+    public function setZipcode($zipcode): void
+    {
+        $this->zipcode = $zipcode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress01()
+    {
+        return $this->address01;
+    }
+
+    /**
+     * @param mixed $address01
+     */
+    public function setAddress01($address01): void
+    {
+        $this->address01 = $address01;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress02()
+    {
+        return $this->address02;
+    }
+
+    /**
+     * @param mixed $address02
+     */
+    public function setAddress02($address02): void
+    {
+        $this->address02 = $address02;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAreaId()
+    {
+        return $this->areaId;
+    }
+
+    /**
+     * @param mixed $areaId
+     */
+    public function setAreaId($areaId): void
+    {
+        $this->areaId = $areaId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrefecture()
+    {
+        return $this->prefecture;
+    }
+
+    /**
+     * @param mixed $prefecture
+     */
+    public function setPrefecture($prefecture): void
+    {
+        $this->prefecture = $prefecture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelNo()
+    {
+        return $this->telNo;
+    }
+
+    /**
+     * @param mixed $telNo
+     */
+    public function setTelNo($telNo): void
+    {
+        $this->telNo = $telNo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFaxNo()
+    {
+        return $this->faxNo;
+    }
+
+    /**
+     * @param mixed $faxNo
+     */
+    public function setFaxNo($faxNo): void
+    {
+        $this->faxNo = $faxNo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMailAddress()
+    {
+        return $this->mailAddress;
+    }
+
+    /**
+     * @param mixed $mailAddress
+     */
+    public function setMailAddress($mailAddress): void
+    {
+        $this->mailAddress = $mailAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSiteUrl()
+    {
+        return $this->siteUrl;
+    }
+
+    /**
+     * @param mixed $siteUrl
+     */
+    public function setSiteUrl($siteUrl): void
+    {
+        $this->siteUrl = $siteUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInsertDate()
+    {
+        return $this->insertDate;
+    }
+
+    /**
+     * @param mixed $insertDate
+     */
+    public function setInsertDate($insertDate): void
+    {
+        $this->insertDate = $insertDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInsertUserId()
+    {
+        return $this->insertUserId;
+    }
+
+    /**
+     * @param mixed $insertUserId
+     */
+    public function setInsertUserId($insertUserId): void
+    {
+        $this->insertUserId = $insertUserId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdateDate()
+    {
+        return $this->updateDate;
+    }
+
+    /**
+     * @param mixed $updateDate
+     */
+    public function setUpdateDate($updateDate): void
+    {
+        $this->updateDate = $updateDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdateUserId()
+    {
+        return $this->updateUserId;
+    }
+
+    /**
+     * @param mixed $updateUserId
+     */
+    public function setUpdateUserId($updateUserId): void
+    {
+        $this->updateUserId = $updateUserId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeleteFlag()
+    {
+        return $this->deleteFlag;
+    }
+
+    /**
+     * @param mixed $deleteFlag
+     */
+    public function setDeleteFlag($deleteFlag): void
+    {
+        $this->deleteFlag = $deleteFlag;
+    }
+}
