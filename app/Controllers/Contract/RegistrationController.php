41bf96ddf13cf5afc4adcc47b53d2927bae88972
Zaman <motaharz95@gmail.com>
2021-05-06 14:05:41 +0600

    Controller works for contract product update

--- app/Controllers/Contract/RegistrationController.php
+++ app/Controllers/Contract/RegistrationController.php
@@ -168,6 +168,78 @@ class RegistrationController extends BaseController
     public function updateAction(){
         if( session() && session()->get('login') ){
             if($this->request->isAJAX()){
+                $contract = new Contract();
+
+                if($_POST['shop']){
+                    $shop = new Shop();
+                    $shop->setId((new SequenceModel())->getShopSequence());
+                    $shop->setName($_POST['shopName'] ?? null);
+                    $shop->setNameKana($_POST['shopNameKana'] ?? null);
+                    $shop->setRepresentative($_POST['shopRepresentative'] ?? null);
+                    $shop->setRepresentativeKana($_POST['shopRepresentativeKana'] ?? null);
+                    $shop->setZipcode($_POST['shopZip'] ?? null);
+                    $shop->setAddress01($_POST['shopAddress01'] ?? null);
+                    $shop->setAddress02($_POST['shopAddress02'] ?? null);
+                    $shop->setAreaId($_POST['shopArea'] ?? null);
+                    $shop->setPrefecture($_POST['shopPrefecture'] ?? null);
+                    $shop->setDistrict($_POST['shopDistrict'] ?? null);
+                    $shop->setAreaLarge($_POST['shopAreaLarge'] ?? null);
+                    $shop->setAreaSmall($_POST['shopAreaSmall'] ?? null);
+                    $shop->setTelNo($_POST['shopTel'] ?? null);
+                    $shop->setMailAddress($_POST['shopMail'] ?? null);
+                    $shop->setSiteUrl($_POST['shopSite'] ?? null);
+                    $shop->setInsertDate(date("Y-m-d H:i:s"));
+                    $shop->setInsertUserId(session()->get('userId'));
+                    $shop->setUpdateDate(date("Y-m-d H:i:s"));
+                    $shop->setUpdateUserId(session()->get('userId'));
+                    $shop->setDeleteFlag(1);
+
+                    (new ShopModel())->storeShopData($shop);
+                    $contract->setShopId($shop->getId());
+                }
+                else{
+                    $contract->setShopId($_POST['shopId']);
+                }
+
+                $contract->setId($_POST['contractId']);
+                $contract->setContractorId($_POST['contractorId'] ?? null);
+                $contract->setTantouId("abcd");
+                $contract->setNote($_POST['note'] ?? null);
+                $contract->setUpdateDate(date("Y-m-d H:i:s"));
+                $contract->setUpdateUserId(session()->get('userId'));
+
+                (new contractmodel())->updateContractData($contract);
+
+                $products = $_POST['productSelectId'];
+                $contractProduct = array();
+                $contractProduct['id'] = $contract->getId();
+                $contractProduct['contractStatus'] = 0;
+                $contractProduct['tantou'] = "abcd";
+                $contractProduct['update'] = date("Y-m-d H:i:s");
+                $contractProduct['updateUser'] = session()->get('userId');
+
+                for ($i = 0; $i < count($products); $i++){
+                    $product = $products[$i];
+                    $productStore = (new ContractModel())->getContractProductById($contract->getId(), $product[0]);
+
+                    if(isset($productStore) && count($productStore) > 0){
+                        continue;
+                    }
+                    else{
+                        $start = explode("-",$product[2]);
+                        $end = explode("-",$product[3]);
+
+                        $contractProduct['product'] = $product[0];
+                        $contractProduct['note'] = $product[1];
+                        $contractProduct['startYear'] = $start[0];
+                        $contractProduct['startMonth'] = $start[1];
+                        $contractProduct['endYear'] = $end[0];
+                        $contractProduct['endMonth'] = $end[1];
+                        $contractProduct['branch'] = strtotime(date("Y-m-d H:i:s")) + $i;
+
+                        (new ContractModel())->storeContractProductData($contractProduct);
+                    }
+                }
                 return json_encode(['msg' => "Successful", 'status' => 1]);
             }
             else{
