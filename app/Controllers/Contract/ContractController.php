0983d2d791c3d84d1840b6f8949df83160d662a1
Zaman <motaharz95@gmail.com>
2021-05-10 10:37:54 +0600

    Basic Controller works to show Contract Search UI

--- app/Controllers/Contract/ContractController.php
+++ app/Controllers/Contract/ContractController.php
@@ -4,11 +4,29 @@
 namespace App\Controllers\Contract;
 
 
+use App\Models\Contract\ContractModel;
+
 class ContractController
 {
-    public function contractorSearch(){
+    public function contractSearch(){
         if( session() && session()->get('login') ){
-            echo "Hello, This section is under developing";
+            if($_SERVER['QUERY_STRING']){
+                $searchId = $_GET['searchById'];
+                $searchName = $_GET['searchByName'];
+                if($searchId){
+                    $contract = (new ContractModel())->getContractDetailsById($searchId);
+                    return "This Section is under Development";
+                    //return view("contract/contractDetail", ["title" => "Contract Details", "contract" => $contract]);
+                }
+                else{
+                    $contract = (new ContractModel())->getContractByName($searchName);
+                    return view("contract/contractSearch", ["title" => "Contract Search", "contract" => $contract]);
+                }
+            }
+            else{
+                $contract = (new ContractModel())->getAllContractData();
+                return view("contract/contractSearch", ["title" => "Contract Search", "contract" => $contract]);
+            }
         }
         else{
             return redirect()->to("/login");
