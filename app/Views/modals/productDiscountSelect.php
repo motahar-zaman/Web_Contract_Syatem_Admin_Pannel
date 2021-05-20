b4e7417b9f0db63efa58cd2917c1d74c91ae374d
XI <khchow@xandi.co.jp>
2021-05-20 11:54:20 +0600

    clean-up

--- app/Views/modals/productDiscountSelect.php
+++ app/Views/modals/productDiscountSelect.php
@@ -12,20 +12,18 @@
                 <div class="card-body table-responsive p-0 ml-3" style="height: 300px;">
                     <table class="table table-head-fixed text-nowrap productDiscountTableModal ml-3">
                         <thead>
-                        <tr>
-                            <th>商品ID</th>
-                            <th>商品名</th>
-                            <th>商品概要</th>
-                            <th>公開開始日</th>
-                            <th>公開終了日</th>
-                        </tr>
+                            <tr>
+                                <th>商品ID</th>
+                                <th>商品名</th>
+                                <th>商品概要</th>
+                                <th>公開開始日</th>
+                                <th>公開終了日</th>
+                            </tr>
                         </thead>
                         <tbody>
-
                         </tbody>
                     </table>
                 </div>
-<!--                <button type="submit" class="btn btn-primary ml-3 mt-5">選択反映</button>-->
             </div>
         </div>
     </div>
