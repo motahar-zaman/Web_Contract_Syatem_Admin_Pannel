5f7faf901278e9336772084ef7ceb00cbb2b6b3c
XI <khchow@xandi.co.jp>
2021-05-21 10:08:41 +0600

    Contractor Search UI change like Contract Search

--- app/Views/Contractor/contractorSearch.php
+++ app/Views/Contractor/contractorSearch.php
@@ -38,66 +38,113 @@
                         </div>
                         <div class="row col-md-12">
                             <div class="col-md-6">
-                                <h1 class="mb-0 pt-2 pl-5">契約者選択</h1>
+                                <h1 class="mb-0 pt-2 pl-5">契約検索・一覧</h1>
                             </div>
                             <div class="col-md-6 text-right pt-4 pr-5">
-                                <span>[<?= session()->get("userId")?>]：[<?= session()->get("userName")?>]</span>
+                                <span>[<?= session()->get("userId") ?>]：[<?= session()->get("userName") ?>]</span>
                             </div>
                         </div>
                     </div>
                     <div class="underline mt-2"></div>
-                    <div class="modal-body">
-                        <form>
-                            <div class="card-body">
-                                <div class="form-group col-md-4 pl-0">
-                                    <label for="exampleInputEmail1">契約者ID（完全一致）</label>
-                                    <input type="text" class="form-control" id="searchById", name="searchById" placeholder="">
-                                </div>
-                                <div class="form-group col-md-4 pl-0">
-                                    <label for="exampleInputPassword1">契約者名（あいまい）</label>
-                                    <input type="text" class="form-control" id="searchByName" name="searchByName" placeholder="">
-                                </div>
-                                <button type="submit" id="contractorSearch" class="btn btn-primary pl-4 pr-4 k1Btn mr-2">検索</button>
-                                <button type="button" id="clearSearchText" class="btn btn-primary ml-2 k1Btn">条件クリア</button>
+
+                    <div class="gap-2 mx-auto " style="max-width: 950px !important;">
+                        <div class="card mt-5 text-left">
+                            <div class="card-header">
+                                <h3 class="card-title text-center">
+                                    【検索条件】
+                                </h3>
                             </div>
-                        </form>
-                        <br />
-                        <div class="card-body table-responsive p-0 ml-1">
-                            <table class="table table-head-fixed text-nowrap ml-1">
-                                <thead>
-                                <tr>
-                                    <th class="d-none">選択</th>
-                                    <th>契約者ID</th>
-                                    <th>契約者名	</th>
-                                    <th>住所</th>
-                                    <th>電話番号</th>
-                                    <th>メールアドレス</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                if(isset($contractor) && count($contractor) > 0){
-                                    for($i = 0; $i < count($contractor); $i++){
-                                        $data = $contractor[$i];
-                                        ?>
-                                        <tr>
-                                            <td class="d-none" onclick="selectedContractor(<?php echo $i ?>)" id="selectedContractor<?php echo $data->getId() ?>"><a href="#">選択</a></td>
-                                            <td id="contractorId<?php echo $i ?>"><a href='<?php echo base_url();?>/contractor-details/<?php echo $data->getId() ?>'><?php echo $data->getId() ?></a></td>
-                                            <td id="contractorName<?php echo $i ?>"><?php echo $data->getName() ?></td>
-                                            <td id="contractorAddress1<?php echo $i ?>"><?php echo $data->getAddress01() ?></td>
-                                            <td id="contractorPhn<?php echo $i ?>"><?php echo $data->getTelNo() ?></td>
-                                            <td id="contractorMail<?php echo $i ?>"><?php echo $data->getMailAddress() ?></td>
-                                        </tr>
-                                        <?php
-                                    }
-                                }
-                                else{
-                                    echo "<h3>No data available</h3>";
-                                }
-                                ?>
-                                </tbody>
-                            </table>
+                            <form>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group " >
+                                                <label>契約者ID（完全一致）</label>
+                                                <input type="text" class="form-control" id="searchById", name="searchById" placeholder="">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>契約者名（あいまい）</label>
+                                                <input type="text" class="form-control" id="searchByName" name="searchByName" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group " >
+                                                <label>店舗ID</label>
+                                                <input type="text" class="form-control " name="shopIdSearch" id="shopIdSearch">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>店舗名</label>
+                                                <input type="text" class="form-control " name="shopNameSearch" id="shopNameSearch">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group " >
+                                                <label>契約者名</label>
+                                                <input type="text" class="form-control " name="contractorNameSearch" id="contractorNameSearch">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>商品名</label>
+                                                <input type="text" class="form-control " name="productNameSearch" id="productNameSearch">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button type="submit" id="contractSearchBtn" class="btn btn-primary text-center k1Btn k1Btn2 mr-3">検索</button>
+                                    <button type="button" id="clearSearchText" class="btn btn-primary ml-2 k1Btn k1Btn2">条件クリア</button>
+                                </div>
+                            </form>
                         </div>
+                    </div>
+
+                    <div class="gap-2 mx-auto text-center" style="max-width: 950px">
+                        <div class="card mt-5 text-left">
+                            <div class="card-header">
+                                <h3 class="card-title text-center">【契約一覧】</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="card-body table-responsive p-0">
+                                    <table class="table table-hover text-center productTable productInfoTable" style="width: 130% !important;">
+                                        <thead>
+                                            <tr>
+                                                <th class="d-none">選択</th>
+                                                <th>契約者ID</th>
+                                                <th>契約者名	</th>
+                                                <th>住所</th>
+                                                <th>電話番号</th>
+                                                <th>メールアドレス</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            if(isset($contractor) && count($contractor) > 0){
+                                                for($i = 0; $i < count($contractor); $i++){
+                                                    $data = $contractor[$i];
+                                                    ?>
+                                                    <tr>
+                                                        <td class="d-none" onclick="selectedContractor(<?php echo $i ?>)" id="selectedContractor<?php echo $data->getId() ?>"><a href="#">選択</a></td>
+                                                        <td id="contractorId<?php echo $i ?>"><a href='<?php echo base_url();?>/contractor-details/<?php echo $data->getId() ?>'><?php echo $data->getId() ?></a></td>
+                                                        <td id="contractorName<?php echo $i ?>"><?php echo $data->getName() ?></td>
+                                                        <td id="contractorAddress1<?php echo $i ?>"><?php echo $data->getAddress01() ?></td>
+                                                        <td id="contractorPhn<?php echo $i ?>"><?php echo $data->getTelNo() ?></td>
+                                                        <td id="contractorMail<?php echo $i ?>"><?php echo $data->getMailAddress() ?></td>
+                                                    </tr>
+                                                    <?php
+                                                }
+                                            }
+                                            else{
+                                                echo "<h3>No data available</h3>";
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="gap-2 mx-auto mt-3 mb-4 " style="max-width: 950px !important;">
+                        <button onclick="" id="menu" class="btn btn-primary pl-3 pr-3 k1Btn k1Btn2">
+                            <a class="k1Btn2" href="/home">メニュー</a>
+                        </button>
                     </div>
                 </div>
             </section>
