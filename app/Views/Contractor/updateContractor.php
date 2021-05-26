fd0fc02b455340cf8531bf1506c918858606710f
XI <khchow@xandi.co.jp>
2021-05-26 11:28:46 +0600

    contractor view files link and script link correction due to directory changed to views/Contractor

--- app/Views/Contractor/updateContractor.php
+++ app/Views/Contractor/updateContractor.php
@@ -9,20 +9,20 @@
         <link rel="stylesheet"
               href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="../template/plugins/fontawesome-free/css/all.min.css">
+        <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
         <!-- daterange picker -->
-        <link rel="stylesheet" href="../template/plugins/daterangepicker/daterangepicker.css">
+        <link rel="stylesheet" href="../..//plugins/daterangepicker/daterangepicker.css">
         <!-- iCheck for checkboxes and radio inputs -->
-        <link rel="stylesheet" href="../template/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+        <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
         <!-- Bootstrap Color Picker -->
-        <link rel="stylesheet" href="../template/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
+        <link rel="stylesheet" href="../../plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
         <!-- Tempusdominus Bootstrap 4 -->
-        <link rel="stylesheet" href="../template/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+        <link rel="stylesheet" href="../../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
         <!-- Select2 -->
-        <link rel="stylesheet" href="../template/plugins/select2/css/select2.min.css">
-        <link rel="stylesheet" href="../template/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+        <link rel="stylesheet" href="../../plugins/select2/css/select2.min.css">
+        <link rel="stylesheet" href="../../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
         <!-- Bootstrap4 Duallistbox -->
-        <link rel="stylesheet" href="../template/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+        <link rel="stylesheet" href="../../plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
         <!-- Theme style -->
         <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
         <link rel="stylesheet" href="css/style.css">
@@ -292,24 +292,24 @@
 
         <script type="text/javascript" src="../../js/contractorUpdate.js"></script>
         <!-- jQuery -->
-        <script src="../template/plugins/jquery/jquery.min.js"></script>
+        <script src="../../plugins/jquery/jquery.min.js"></script>
         <!-- Bootstrap 4 -->
-        <script src="../template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
         <!-- Select2 -->
-        <script src="../template/plugins/select2/js/select2.full.min.js"></script>
+        <script src="../../plugins/select2/js/select2.full.min.js"></script>
         <!-- Bootstrap4 Duallistbox -->
-        <script src="../template/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+        <script src="../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
         <!-- InputMask -->
-        <script src="../template/plugins/moment/moment.min.js"></script>
-        <script src="../template/plugins/inputmask/jquery.inputmask.min.js"></script>
+        <script src="../../plugins/moment/moment.min.js"></script>
+        <script src="../../plugins/inputmask/jquery.inputmask.min.js"></script>
         <!-- date-range-picker -->
-        <script src="../template/plugins/daterangepicker/daterangepicker.js"></script>
+        <script src="../../plugins/daterangepicker/daterangepicker.js"></script>
         <!-- bootstrap color picker -->
-        <script src="../template/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+        <script src="../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
         <!-- Tempusdominus Bootstrap 4 -->
-        <script src="../template/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+        <script src="../../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
         <!-- Bootstrap Switch -->
-        <script src="../template/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+        <script src="../../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
         <!-- AdminLTE App -->
         <script src="../../dist/js/adminlte.min.js"></script>
         <!-- AdminLTE for demo purposes -->
