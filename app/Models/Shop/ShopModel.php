874c2729246b7c639af820bcd8725d9f80badf5c
XI <khchow@xandi.co.jp>
2021-06-01 17:37:33 +0600

    shopModel correction for all query and mapping method according to shop class

--- app/Models/Shop/ShopModel.php
+++ app/Models/Shop/ShopModel.php
@@ -12,8 +12,6 @@ class ShopModel
         $id = $shop->getId();
         $name = $shop->getName();
         $nameKana = $shop->getNameKana();
-        $representative = $shop->getRepresentative();
-        $representativeKana = $shop->getRepresentativeKana();
         $zip = $shop->getZipcode();
         $address1 = $shop->getAddress01();
         $address2 = $shop->getAddress02();
@@ -26,18 +24,17 @@ class ShopModel
         $fax = $shop->getFaxNo();
         $mail = $shop->getMailAddress();
         $siteUrl = $shop->getSiteUrl();
-        $notification = $shop->getNotificationLetter();
         $update = $shop->getUpdateDate();
         $updateUser = $shop->getUpdateUserId();
         $insert = $shop->getInsertDate();
         $insertUser = $shop->getInsertUserId();
         $delete = $shop->getDeleteFlag();
 
-        $queryString = "INSERT INTO mst_shop (shop_id, shop_name, shop_name_kana, daihyousha_name, daihyousha_name_kana, zipcode, address_01, address_02,
-                        area_id, prefecture, district_id, large_area_id, small_area_id, tel_no, fax_no, mail_address, site_url, notification_letter, update_date,
-                        update_user_id, insert_date, insert_user_id, delete_flag) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $queryParameter = array($id, $name, $nameKana, $representative, $representativeKana, $zip, $address1, $address2, $area, $prefecture, $district,
-            $areaLarge, $areaSmall, $phn, $fax, $mail, $siteUrl, $notification, $update, $updateUser, $insert, $insertUser, $delete);
+        $queryString = "INSERT INTO mst_shop (shop_id, shop_name, shop_name_kana, zipcode, address_01, address_02, area_id, prefecture,
+                        district_id, large_area_id, small_area_id, tel_no, fax_no, mail_address, site_url, update_date, update_user_id,
+                        insert_date, insert_user_id, delete_flag) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $queryParameter = array($id, $name, $nameKana, $zip, $address1, $address2, $area, $prefecture, $district, $areaLarge, $areaSmall,
+            $phn, $fax, $mail, $siteUrl, $update, $updateUser, $insert, $insertUser, $delete);
 
         return (new Database())->writeQueryExecution($queryString, $queryParameter);
     }
@@ -48,9 +45,9 @@ class ShopModel
     }
 
     public function getAllData(){
-        $queryString = "SELECT shop_id, shop_name, shop_name_kana, daihyousha_name, daihyousha_name_kana, zipcode, address_01, address_02, area_id,
-                        prefecture, district_id, large_area_id, small_area_id, tel_no, fax_no, mail_address, site_url, notification_letter, update_date,
-                        update_user_id, insert_date, insert_user_id, delete_flag FROM mst_shop WHERE delete_flag = ? ORDER BY update_date DESC";
+        $queryString = "SELECT shop_id, shop_name, shop_name_kana, zipcode, address_01, address_02, area_id, prefecture, district_id,
+                        large_area_id, small_area_id, tel_no, fax_no, mail_address, site_url, update_date, update_user_id, insert_date,
+                        insert_user_id, delete_flag FROM mst_shop WHERE delete_flag = ? ORDER BY update_date DESC";
         $queryParameter = array(1);
 
         return (new Database())->readQueryExecution($queryString, $queryParameter);
@@ -68,8 +65,6 @@ class ShopModel
                     $shop->setId($data->shop_id ?? NULL);
                     $shop->setName($data->shop_name ?? NULL);
                     $shop->setNameKana($data->shop_name_kana ?? NULL);
-                    $shop->setRepresentative($data->daihyousha_name ?? NULL);
-                    $shop->setRepresentativeKana($data->daihyousha_name_kana ?? NULL);
                     $shop->setZipCode($data->zipcode ?? NULL);
                     $shop->setAddress01($data->address_01 ?? NULL);
                     $shop->setAddress02($data->address_02 ?? NULL);
@@ -82,7 +77,6 @@ class ShopModel
                     $shop->setFaxNo($data->fax_no ?? NULL);
                     $shop->setMailAddress($data->mail_address ?? NULL);
                     $shop->setSiteUrl($data->site_url ?? NULL);
-                    $shop->setNotificationLetter($data->notification_letter ?? NULL);
                     $shop->setUpdateDate($data->update_date ?? NULL);
                     $shop->setUpdateUserId($data->update_user_id ?? NULL);
                     $shop->setInsertDate($data->insert_date ?? NULL);
