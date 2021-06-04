b916f68d72d2576d9b73bf8289ec2ee431770aa3
Maizied <mdshuvo40@gmail.com>
2021-06-04 14:50:51 +0600

    Added Discount Modal.

---
+++ app/Views/modals/discountModal.php
@@ -0,0 +1,113 @@
+<div class="modal fade" id="contractor-select-modal">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">割引サービス</h4>
+                <button id="contractorModalClose" type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="card-body">
+                        <div class="form-group col-md-4 pl-0">
+                            <label>稟議No（完全一致）    Ringi No (Exact match)</label>
+                            <input type="text" class="form-control" placeholder="" value="">
+                        </div>
+                        <span id="productSearch" class="btn btn-primary pl-4 pr-4 k1Btn k1Btn2 mr-3">検索(Search)</span>
+                    </div>
+                </form>
+                <br />
+
+
+                <div class="card mt-3 text-left">
+                    <div class="card-body">
+                        <div class="row">
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label>契約種別(Contract Type)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>対象区分(Target classification)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>対象名(Target Name)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>内容項目(Content Item)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>内容詳細(Content detail)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>条件(Condition)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12  mt-3">
+                                <label>税抜き価格 :</label>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group " >
+                                    <label>サービス前</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>サービス前</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12 mt-3">
+                                <label>摘要期間(Applicable period) :</label>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>月数</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>開始日</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>終了日</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label>目標(Purpose)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>備考(Remarks)</label>
+                                    <span class="form-control" ></span>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary ml-3 mt-2 k1Btn k1Btn2" style="margin-left: 5px !important;" data-dismiss="modal" aria-label="Close">摘要(Apply)</button>
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
