9aadd9397046dbcb7a4a15fe24b999570c8907e2
XI <khchow@xandi.co.jp>
2021-06-09 15:49:49 +0600

    Controller method to get prefecture subordinate address route

--- app/Controllers/Address/AddressController.php
+++ app/Controllers/Address/AddressController.php
@@ -30,4 +30,23 @@ class AddressController extends BaseController
                 return json_encode(['msg' => "Not Logged in user", 'status' => 3]);
             }
     }
+
+    public function getPrefectureSubordinateAddress($districtId, $prefectureId){
+        if( session() && session()->get('login') ) {
+            $areaLarge = (new AddressModel())->getAreaLargeData($districtId, $prefectureId);
+            $areaSmall = (new AddressModel())->getAreaSmallData($districtId, $prefectureId);
+            $area = (new AddressModel())->getAreaData($districtId, $prefectureId);
+
+            $data = array(
+                "areaLarge" => $areaLarge,
+                "areaSmall" => $areaSmall,
+                "area" => $area
+            );
+
+            return json_encode(['msg' => "Successful", $data, "status" => 1]);
+        }
+        else{
+                return json_encode(['msg' => "Not Logged in user", 'status' => 3]);
+            }
+    }
 }
