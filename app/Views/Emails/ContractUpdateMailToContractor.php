2a1302e307a528b33317a7f08aa0adf0f1257d54
XI <khchow@xandi.co.jp>
2021-06-14 17:07:59 +0600

    Create contract update email file for Contractor

---
+++ app/Views/Emails/ContractUpdateMailToContractor.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Email for Contract Update</title>
+    </head>
+    <body>
+        <div>
+            <header>Contract Update</header><br><br>
+            <span class="mt-5 mb-5">Contract is updated By <?= $user ?>, ContractId is <?= $contractId ?></span>
+            <footer>Web_Contract_Syatems</footer><br>
+        </div>
+    </body>
+</html>
